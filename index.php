2dce791627abb0313cad4351f28672f9b69d0af8
sebasare <61071641+sebasare@users.noreply.github.com>
2020-02-21 21:15:09 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
 <?php
-    echo("hola mundo");
+
+   
     
 
 ?>
+
+<HTMl:5>
+    <head>
+    <TItle>taller 1</TItle>
+    </head>
+    <body>
+        <fieldset>
+        <legend>operaciones matematicas</legend>
+        <form action="#" method="post"></form>
+        <label for="n1">primer numero</label>
+        <input type="number"name='n1'required>
+        <br><br>
+        <label for="n2">segundo numero</label>
+        <input type="number"name='n2'required>
+        <br><br>
+        <label for="operaciones aritmeticas"></label>
+        <select name="operaciones" id="opr">
+        <option value="sumar">sumar</option>
+        <option value="restar">restar</option>
+        <option value="multiplicar">multiplicar</option>
+        <option value="dividir">dividir</option>
+        </select>
+        
+
+        </fieldset>
+    </body>
+    
+</HTMl:5>
